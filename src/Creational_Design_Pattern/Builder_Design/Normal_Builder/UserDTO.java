f0e79ff27a7eff2526fb762480e6fd90f2c41f15
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2022-06-12 15:38:50 +0530

    Builder design pattern-1

---
+++ src/Creational_Design_Pattern/Builder_Design/Normal_Builder/UserDTO.java
@@ -0,0 +1,11 @@
+package Creational_Design_Pattern.Builder_Design.Normal_Builder;
+
+//Interface implemented by "products"
+public interface UserDTO {
+	
+	String getName();
+
+	String getAddress();
+
+	String getAge();
+}
