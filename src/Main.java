e325607544a6fd98ebcf1e3037fa36c1e3fc7c61
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2023-12-31 19:29:51 +0530

    state design pattern UML

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> set = new HashSet<>();
-        Stream<Integer> stream = Stream.of(5,6,7,8,9,5,10,7);
 
-        List list = stream.filter(x -> !set.add(x)).toList();
-        System.out.println(list.toString());
     }
 }
